10525d7d027ef9c6381e0b910b7e0404787c8c13
Andrea Baccega <vekexasia@gmail.com>
2018-06-06 10:40:06 +0200

    Allow seed peer to be offline. and db script for dev purposes.

--- .devutils/startdb.sh
+++ .devutils/startdb.sh
@@ -1,2 +1,2 @@
-docker run --rm --name pg -e POSTGRES_PASSWORD=password -e POSTGRES_USER=rise -e POSTGRES_DB=rise_db -p 5432:5432 postgres:9.6-alpine
+docker run --rm --name pg -e POSTGRES_PASSWORD=password -e POSTGRES_USER=oxycoin -e POSTGRES_DB=oxycoin_db_main -p 5432:5432 postgres:9.6-alpine
 
